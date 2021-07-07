82b0a56b50fac555d35176bd623091b73869fd55
EC2 Default User <ec2-user@ip-172-31-15-248.ec2.internal>
2021-07-07 11:14:20 +0000

    [Update]ジャンル完成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   namespace :admins do
   get "/homes/top" => "homes#top", as: "top"
   resources :items
+  resources :genres
+  resources :costomers
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
